86111556162d3c35f5795f2f377db96b87428179
chandureddy08 <chandrareddyerigela@gmail.com>
2024-06-12 14:32:35 -0400

    cicd dev infra

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    type = string
+    default = "expense"
+}
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+        Component = "backend"
+    }
+}
+variable "zone_name" {
+    type = string
+  default = "chandureddy.online"
+}
+variable "app_version" {
+}
